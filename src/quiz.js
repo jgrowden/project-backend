5cd35934e82f87dfc9f40f91df1aed7cc0ee4f47
Andrew <z5487663@ad.unsw.edu.au>
2024-02-20 02:01:00 +1100

    Implemented adminQuizInfo function stubble

---
+++ src/quiz.js
@@ -0,0 +1,9 @@
+function adminQuizInfo(authUserId, quizId) {
+    return {
+        quizId: 1,
+        name: 'My Quiz',
+        timeCreated: 1683125870,
+        timeLastEdited: 1683125871,
+        description: 'This is my quiz',
+    }
+}
