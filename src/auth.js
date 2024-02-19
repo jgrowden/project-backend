998aad0c1957aad8ae66878b618cbe78245040c3
James Growden <z5362762@ad.unsw.edu.au>
2024-02-20 10:41:55 +1100

    updated to only include adminAuthLogin

--- src/auth.js
+++ src/auth.js
@@ -1,14 +1,4 @@
 /**
- * Register a user with an email, password, and names, 
- * then returns their authUserId value.
- */
-function adminAuthRegister(email, password, nameFirst, nameLast) {
-    return {
-        authUserId: 1,
-    }
-}
-
-/**
  * Given a registered user's email and password,
  * returns their authUserId value.
  */
