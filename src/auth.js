369b200bf89ac94fa1f9f06dc3ff4242d89b7fa2
James Growden <z5362762@ad.unsw.edu.au>
2024-02-20 10:29:06 +1100

    wrote stub for adminAuthRegister

---
+++ src/auth.js
@@ -0,0 +1,9 @@
+/**
+ * Register a user with an email, password, and names, 
+ * then returns their authUserId value.
+ */
+function adminAuthRegister(email, password, nameFirst, nameLast) {
+    return {
+        authUserId: 1,
+    }
+}
