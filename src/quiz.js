c2218935238c2b44ae17eeeb4de7458ac7645d54
Annabelle <z5476222@ad.unsw.edu.au>
2024-02-20 21:17:09 +1100

    changed comment formatting for adminQuizList

--- src/quiz.js
+++ src/quiz.js
@@ -1,11 +1,17 @@
-
 /**
  * Provide a list of all quizzes that are owned by the currently logged in user.
  * 
  * @param {number} authUserId - unique identifier for user
- * @returns {{quizzes: [{quizId: number, name: string}]}} - object with list of all quizzes
-*
-*/  
+ * @returns {{
+*    quizzes: [
+    *       {
+    *          quizId: number, 
+    *          name: string
+    *       }
+    *    ]
+    * }} - object with list of all quizzes
+    *
+    */ 
 
 function adminQuizList(authUserId) {
 
