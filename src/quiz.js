b1740a50e3624659e1fc7efe00f3f53831adafcd
Andrew <z5487663@ad.unsw.edu.au>
2024-02-20 16:08:00 +1100

    added comments

--- src/quiz.js
+++ src/quiz.js
@@ -1,5 +1,11 @@
+/**
+ * Given a particular quiz, permanently remove the quiz.
+ * 
+ * @param {int} authUserId - a user's unique identification number
+ * @param {int} quizId - a quiz's unique identification number
+ * @returns {} - an empty object
+ */
+
 function adminQuizRemove(authUserId, quizId) {
-    return {
-        
-    }
+    return {}
 }
