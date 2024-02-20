cf7c8ac2a702a379b942ed505d4fd3216e21b650
Andrew <z5487663@ad.unsw.edu.au>
2024-02-20 16:04:37 +1100

    added comments

--- src/quiz.js
+++ src/quiz.js
@@ -1,5 +1,15 @@
+/**
+ * Given basic details about a new quiz, create one for the logged in user.
+ * 
+ * @param {int} authUserId - unique user identification number
+ * @param {string} name - name of quiz created
+ * @param {string} description - description of the quiz being created
+ * @returns {quizId: 2} - object with a unique quiz identification number
+*/
+
 function adminQuizCreate(authUserId, name, description) {
     return {
         quizId: 2
     }
 }
+
