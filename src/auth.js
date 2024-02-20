15fcf2ab9c35d3227d54263c87a1ae9b06a2bb9e
James Growden <z5362762@ad.unsw.edu.au>
2024-02-20 18:27:56 +1100

    Redoing adminAuthRegister

--- src/auth.js
+++ src/auth.js
@@ -1,4 +1,18 @@
 /**
+ * Register a user with an email, password, and names, 
+ * then returns their authUserId value.
+ * 
+ * @param {string} email
+ * @param {string} password
+ * @param {string} nameFirst
+ * @param {string} nameLast
+ */
+function adminAuthRegister(email, password, nameFirst, nameLast) {
+    return {
+        authUserId: 1,
+    }
+}
+/**
  * Reset the state of the application back to the start.
  */
 function clear() {
