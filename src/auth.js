43a9f92e8fcbe42ec1a0ea7ee9faab7bccd11f79
Kaelan <z5478371@ad.unsw.edu.au>
2024-02-20 13:04:09 +1100

    implemented adminUserDetailsUpdate function stubble

---
+++ src/auth.js
@@ -0,0 +1,14 @@
+/**
+ * Given an admin user's authUserId and a set of properties, 
+ *  update the properties of this logged in admin user.
+ * 
+ * @param {number} authUserId - unique identifier for user
+ * @param {string} email - user's email
+ * @param {string} nameFirst - user's first name
+ * @param {string} nameLast - user's last name
+ * @return {} // an empty object
+ */
+function adminUserDetailsUpdate(authUserId, email, nameFirst, nameLast) {
+
+    return {};
+}
