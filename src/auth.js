de5dae33bb51b376fd80e26f84f52e0acc0f2385
James Growden <z5362762@ad.unsw.edu.au>
2024-02-20 18:24:29 +1100

    Redoing adminAuthLogin (again)

--- src/auth.js
+++ src/auth.js
@@ -1,4 +1,18 @@
 /**
+ * Given a registered user's email and password,
+ * returns their authUserId value.
+ * 
+ * @param {string} email
+ * @param {string} password
+ * @returns {number} authUserId
+ */
+function adminAuthLogin(email, password) {
+    return {
+        authUserId: 1,
+    }
+}
+
+/**
  * Reset the state of the application back to the start.
  */
 function clear() {
