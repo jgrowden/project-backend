a22bd6cb940e65c9f68620440d1fe39728ebefa8
James Gair <z5360800@ad.unsw.edu.au>
2024-02-21 14:48:37 +1100

    Implemented adminQuizNameUpdate stub

---
+++ src/quiz.js
@@ -0,0 +1,13 @@
+/**
+ * Update the name of the relevant quiz.
+ * 
+ * @param {number} authUserId 
+ * @param {number} quizId 
+ * @param {string} name
+ * @returns {
+* }
+*/
+
+function adminQuizNameUpdate (authUserId, quizId, name) {
+    return { }
+}
