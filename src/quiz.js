78b8d4cdf46560b7e77ab90674d78aae3978dea3
Andrew <z5487663@ad.unsw.edu.au>
2024-02-21 16:28:52 +1100

    testing commit

--- src/quiz.js
+++ src/quiz.js
@@ -1,8 +1,8 @@
 /**
  * Given a particular quiz, permanently remove the quiz.
  * 
- * @param {int} authUserId - a user's unique identification number
- * @param {int} quizId - a quiz's unique identification number
+ * @param {number} authUserId - a user's unique identification number
+ * @param {number} quizId - a quiz's unique identification number
  * @returns {} - an empty object
  */
 
