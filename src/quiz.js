a55b1abb16bf9e40fc5914390aa8b61bd8516b82
James Gair <z5360800@ad.unsw.edu.au>
2024-02-21 14:56:13 +1100

    Implemented adminQuizDescriptionUpdate stub

---
+++ src/quiz.js
@@ -0,0 +1,13 @@
+/**
+ * Update the description of the relevant quiz.
+ * 
+ * @param {number} authUserId 
+ * @param {number} quizId 
+ * @param {string} description
+ * @returns {
+* }
+*/
+
+function adminQuizDescriptionUpdate( authUserId, quizId, description ) {
+    return { }
+}
