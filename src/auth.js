56d801442da97c324defe6e453b27a782e8eeaab
Andrew <z5487663@ad.unsw.edu.au>
2024-02-23 19:18:09 +1100

    Fixed and standardised comments and parameter/return value descriptions.

--- src/auth.js
+++ src/auth.js
@@ -2,13 +2,15 @@
  * Register a user with an email, password, and names, 
  * then returns their authUserId value.
  * 
- * @param {string} email
- * @param {string} password
- * @param {string} nameFirst
- * @param {string} nameLast
- * @returns {number} authUserId
+ * @param {string} email - user's email
+ * @param {string} password - user's password
+ * @param {string} nameFirst - user's first name
+ * @param {string} nameLast - user's last name
+ * 
+ * @returns {number} authUserId - the user's unique identification number 
  */
 function adminAuthRegister(email, password, nameFirst, nameLast) {
+
     return {
         authUserId: 1,
     }
@@ -18,11 +20,13 @@ function adminAuthRegister(email, password, nameFirst, nameLast) {
  * Given a registered user's email and password,
  * returns their authUserId value.
  * 
- * @param {string} email
- * @param {string} password
- * @returns {number} authUserId
+ * @param {string} email - user's email 
+ * @param {string} password - user's password
+ * 
+ * @returns {number} authUserId - the user's unique identification number 
  */
 function adminAuthLogin(email, password) {
+
     return {
         authUserId: 1,
     }
@@ -33,10 +37,11 @@ function adminAuthLogin(email, password) {
  * 
  * "name" is the first and last name concatenated with a single space between them.
  * 
- * authUserId seems to be the input number, while the user object contains just 'userId'.
- * They should be mean the same thing though.
+ * Note: authUserId seems to be the input number, while the user object contains
+ * just 'userId'. They should be mean the same thing though.
  * 
- * @param {number} authUserId 
+ * @param {number} authUserId - unique identifier for user
+ * 
  * @return {{
  *      user: {
  *          userId: number, 
@@ -45,7 +50,7 @@ function adminAuthLogin(email, password) {
  *          numSuccessfulLogins: number, 
  *          numFailedPasswordsSinceLastLogin: number
  *      }
- *   }}
+ *   }} - an object containing key information about the user queried. 
 */
 function adminUserDetails(authUserId) {
 
@@ -68,6 +73,7 @@ function adminUserDetails(authUserId) {
  * @param {string} email - user's email
  * @param {string} nameFirst - user's first name
  * @param {string} nameLast - user's last name
+ * 
  * @return {} - an empty object
 */
 function adminUserDetailsUpdate(authUserId, email, nameFirst, nameLast) {
@@ -81,6 +87,7 @@ function adminUserDetailsUpdate(authUserId, email, nameFirst, nameLast) {
  * @param {number} authUserId - unique identifier for user
  * @param {string} oldPassword - user's old password
  * @param {string} newPassword - user's new password
+ * 
  * @return {} - an empty object
 */    
 
