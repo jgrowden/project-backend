a7684e8ca1e1eed8661fa397929668e565c8ffdb
Annabelle <z5476222@ad.unsw.edu.au>
2024-02-27 14:35:27 +1100

    Wrote tests for adminQuizList (there's still a typeerror floating around, will fix later)

---
+++ src/adminQuizList.test.js
@@ -0,0 +1,59 @@
+import { clear } from './other.js'
+import { adminQuizList, adminAuthRegister, adminQuizCreate } from './quiz.js'
+
+describe('adminQuizList', () => {
+    test('returning list of quizzes', () => {
+        clear();
+        const userId = adminAuthRegister('email@gmail.com', 'p@ssword', 'first_name', 'last_name');
+        const quizId1 = adminQuizCreate(userId, 'quiz1', 'the first quiz');
+        const quizId2 = adminQuizCreate(userId, 'quiz2', 'the second quiz');
+        expect(adminQuizList(userId)).toEqual({
+            quizzes: [
+                {
+                    quizId: quizId1,
+                    name: 'quiz1' ,
+                },
+                {
+                    quizId: quizId2,
+                    name: 'quiz2',
+                }
+            ]
+        });
+        });
+    test('returning list of quizzes for multiple users', () => {
+        clear();
+        const userId1 = adminAuthRegister('email1@gmail.com', 'p@ssword', 'first_name', 'last_name');
+        const userId2 = adminAuthRegister('email2@gmail.com', 'p@ssword', 'first_name', 'last_name');
+        const quizId1 = adminQuizCreate(userId1, 'quiz1', 'the first quiz');
+        const quizId2 = adminQuizCreate(userId2, 'quiz2', 'the second quiz');
+        const quizId3 = adminQuizCreate(userId1, 'quiz3', 'the third quiz');
+        expect(adminQuizList(userId1)).toEqual({
+            quizzes: [
+                {
+                    quizId: quizId1,
+                    name: 'quiz1' ,
+                },
+                {
+                    quizId: quizId3,
+                    name: 'quiz2',
+                }
+            ]
+        });
+        expect(adminQuizList(userId2)).toEqual({
+            quizzes: [
+                {
+                    quizId: quizId2,
+                    name: 'quiz2',
+                }
+            ]
+        });
+    });
+    test('returning empty list of quizzes', () => {
+        clear();
+        const userId = adminAuthRegister('email@gmail.com', 'p@ssword', 'first_name', 'last_name');
+        expect(adminQuizList(userId)).toEqual({
+            quizzes: []
+        });
+    });
+});
+
