e5d4112ba59bd2f67cc88710fc0206f308d20b32
James Growden <z5362762@ad.unsw.edu.au>
2024-02-27 14:12:51 +1100

    corrected and updated test file to be more comprehensive

--- src/tests/adminAuthRegister.test.js
+++ src/tests/adminAuthRegister.test.js
@@ -2,22 +2,48 @@ import { clear } from '../other.js'
 import {getData, setData } from '../dataStore.js'
 import { adminAuthRegister } from '../auth.js'
 
-function test1() {
-    clear();
-    const userId = adminAuthRegister('go.d.usopp@gmail.com', 'sogeking', 'God', 'Usopp');
-    const data = getData();
+clear();
+test('Test successful registration', () => {
+    let authUserId = adminAuthRegister('go.d.usopp@gmail.com', 'S0geking', 'God', 'Usopp');
+    expect(authUserId.authUserId).toBe(1);
+    let data = getData();
     console.log(data.users);
-    console.log("Expected: email: go.d.usopp@gmail.com, password: sogeking, nameFirst: God, nameLast: Usopp");
-    console.assert(userId === data.users.length);
-    console.log("Test 1 Passed")
-}
+});
 
-function test2() {
-    clear();
-    const userId1 = adminAuthRegister('go.d.usopp@gmail.com', 'sogeking', 'God', 'Usopp');
-    const userId2 = adminAuthRegister('tonytony.chopper@gmail.com', 'racoon_dog', 'Tony Tony', 'Chopper');
-    console.log(data.users[1]);
-    console.log("Expected: email: tonytony.chopper@gmail.com, password: racoon_dog, nameFirst: Tony Tony, nameLast: Chopper");
-    console.log(userId1, userId2);
-    console.log("Test 2 Passed")
-}
+test('Check for duplicate email', () => {
+    expect(adminAuthRegister('go.d.usopp@gmail.com', 'S0geking', 'God', 'Usopp'))
+        .toMatchObject({ "error": "User with given email already exists" });
+});
+
+test('Check for invalid nameFirst', () => {
+    expect(adminAuthRegister('tonytony.chopper@gmail.com', 'racoon_dog1', 'Tony 1000', 'Chopper'))
+        .toMatchObject({ "error": "Invalid first name" });
+
+    expect(adminAuthRegister('monkey.d.luffy@gmail.com', 'gomugomu5', 'D', 'Monkey'))
+        .tomatchObject({ "error" : "nameFirst does not exceed minimum character count" });
+
+    expect(adminAuthRegister('monkey.d.luffy@gmail.com', 'gomugomu5', 'DDDDDDDDDDDDDDDDDDDDDDDD', 'Monkey'))
+        .tomatchObject({ "error" : "nameFirst exceeds maximum character count" });
+});
+
+test('Check for invalid nameLast', () => {
+    expect(adminAuthRegister('nico.robin@gmail.com', '0hara_demon', 'Nico', 'R0bin'))
+        .toMatchObject({ "error": "invalid last name" });
+
+    expect(adminAuthRegister('brook@gmail.com', 'S0ul_King', 'Soul', 'K'))
+        .toMatchObject({ "error": "last name does not exceed minimum" });
+
+    expect(adminAuthRegister('franky@gmail.com', 'Thous4nd_sunny', 'Cutty', 'Flammmmmmmmmmmmmmmmmmm')
+        .toMatchObject({ "error": "last name exceeds maximum"}))
+});
+
+test('Check for invalid password', () => {
+    expect(adminAuthRegister('nico.robin@gmail.com', 'ohara_demon', 'Nico', 'Robin'))
+        .toMatchObject({ "error": "password doesn't contain at least one number"});
+
+    expect(adminAuthRegister('nico.robin@gmail.com', '123456789', 'Nico', 'Robin'))
+        .toMatchObject({ "error": "password doesn't contain at least one letter"});
+
+    expect(adminAuthRegister('nico.robin@gmail.com', '0hara', 'Nico', 'Robin'))
+        .toMatchObject({ "error": "password is less than 8 characters"});
+});
