606396227f56d38fe9c4d663605f728a798d77ec
Kaelan <z5478371@ad.unsw.edu.au>
2024-02-28 22:56:52 +1100

    feature + test: Implemented adminAuthRegister and fixed tests

--- src/auth.js
+++ src/auth.js
@@ -136,16 +136,29 @@ function adminAuthLogin(email, password) {
  *      }
  *   }} - an object containing key information about the user queried. 
 */
-function adminUserDetails(authUserId) {
+export function adminUserDetails(authUserId) {
+
+    if (typeof authUserId !== "number") {
+        return {error: "Invalid User ID Provided"};
+    }
+
+    const data = getData();
+    for (let user of data.users) {
+        if (user.authUserId === authUserId) {
+            return {
+                user: {
+                    userId: user.authUserId,
+                    name: `${user.nameFirst} ${user.nameLast}`,
+                    email: user.email,
+                    numSuccessfulLogins: user.numSuccessfulLogins,
+                    numFailedPasswordsSinceLastLogin: user.numFailedPasswordsSinceLastLogin,
+                },
+            };
+        }
+    }
 
     return {
-        user: {
-            userId: 1,
-            name: 'Hayden Smith',
-            email: 'hayden.smith@unsw.edu.au',
-            numSuccessfulLogins: 3,
-            numFailedPasswordsSinceLastLogin: 1,
-        },
+        error: "User ID not found",
     };
 }
 
