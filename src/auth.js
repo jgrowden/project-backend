878ec2b5feea4c2a29dc8c99ea607e3d30b5b216
Kaelan <z5478371@ad.unsw.edu.au>
2024-02-28 21:51:55 +1100

    fix: change keys in database object to camelcase

--- src/auth.js
+++ src/auth.js
@@ -55,7 +55,7 @@ export function adminAuthRegister(email, password, nameFirst, nameLast) {
         password: password,
         nameFirst: nameFirst,
         nameLast: nameLast,
-        AuthUserId: data.users.length + 1,
+        authUserId: data.users.length + 1,
         numSuccessfulLogins: 1,
         numFailedPasswordsSinceLastLogin: 0,
         userQuizzes: [],
