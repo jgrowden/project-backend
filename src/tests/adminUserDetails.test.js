0c8afb126e77c30fcb9fbd96bca332c9523f8f7b
Kaelan <z5478371@ad.unsw.edu.au>
2024-02-28 23:08:17 +1100

    feature + test: finished implementing adminAuthRegister and fixed tests

--- src/tests/adminUserDetails.test.js
+++ src/tests/adminUserDetails.test.js
@@ -1,27 +1,33 @@
 import { clear } from '../other.js';
 import { adminAuthRegister, adminUserDetails } from '../auth.js';
 
-
-clear();
-
 describe('adminUserDetails testing', () => {
     test('empty database', () => {
-        expect(adminUserDetails(123456789)).toHaveProperty(error);
+        clear();
+        expect(adminUserDetails(123456789)).toHaveProperty("error");
     });
     test('no authUserId provided', () => {
-        expect(adminUserDetails()).toHaveProperty(error);
+        clear();
+        expect(adminUserDetails()).toHaveProperty("error");
     });
     test('authUserId does not exist', () => {
+        clear();
         const user1 = adminAuthRegister('go.d.usopp@gmail.com', 'S0geking', 'God', 'Usopp');
         const user2 = adminAuthRegister('doffy@gmail.com', 'String-Str1ng', 'Donquixote', 'Doflamingo');
-        expect(adminUserDetails(3)).toHaveProperty(error);
+        expect(adminUserDetails(3)).toHaveProperty("error");
     });
     test('valid authUserId 1', () => {
+        clear();
+        const user1 = adminAuthRegister('go.d.usopp@gmail.com', 'S0geking', 'God', 'Usopp');
+        const user2 = adminAuthRegister('doffy@gmail.com', 'String-Str1ng', 'Donquixote', 'Doflamingo');
         expect(adminUserDetails(1)).toMatchObject({user: {userId: 1, name: 'God Usopp', email: "go.d.usopp@gmail.com", 
-                                                    numSuccessfulLogins: 0, numFailedPasswordsSinceLastLogin: 0}});
+                                                    numSuccessfulLogins: 1, numFailedPasswordsSinceLastLogin: 0}});
     });
     test('valid authUserId 2', () => {
+        clear();
+        const user1 = adminAuthRegister('go.d.usopp@gmail.com', 'S0geking', 'God', 'Usopp');
+        const user2 = adminAuthRegister('doffy@gmail.com', 'String-Str1ng', 'Donquixote', 'Doflamingo');
         expect(adminUserDetails(2)).toMatchObject({user: {userId: 2, name: "Donquixote Doflamingo", email: "doffy@gmail.com", 
-                                                    numSuccessfulLogins: 0, numFailedPasswordsSinceLastLogin: 0}});
+                                                    numSuccessfulLogins: 1, numFailedPasswordsSinceLastLogin: 0}});
     });
 });
