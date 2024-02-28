f6bb4354831818e602e9e771c7ba09db17c3032f
Kaelan <z5478371@ad.unsw.edu.au>
2024-02-28 15:26:07 +1100

    tests for adminUserDetails

---
+++ src/tests/adminUserDetails.test.js
@@ -0,0 +1,27 @@
+import { clear } from '../other.js';
+import { adminAuthRegister, adminUserDetails } from '../auth.js';}
+
+
+clear();
+
+describe('adminUserDetails testing', () => {
+    test('empty database', () => {
+        expect(adminUserDetails(123456789)).toHaveProperty(error);
+    });
+    test('no authUserId provided', () => {
+        expect(adminUserDetails()).toHaveProperty(error);
+    });
+    test('authUserId does not exist', () => {
+        const user1 = adminAuthRegister('go.d.usopp@gmail.com', 'S0geking', 'God', 'Usopp');
+        const user2 = adminAuthRegister('doffy@gmail.com', 'String-Str1ng', 'Donquixote', 'Doflamingo');
+        expect(adminUserDetails(3)).toHaveProperty(error);
+    });
+    test('valid authUserId 1', () => {
+        expect(adminUserDetails(1)).toMatchObject({user: {userId: 1, name: 'God Usopp', email: "go.d.usopp@gmail.com", 
+                                                    numSuccessfulLogins: 0, numFailedPasswordsSinceLastLogin: 0}});
+    });
+    test('valid authUserId 2', () => {
+        expect(adminUserDetails(2)).toMatchObject({user: {userId: 2, name: "Donquixote Doflamingo", email: "doffy@gmail.com", 
+                                                    numSuccessfulLogins: 0, numFailedPasswordsSinceLastLogin: 0}});
+    });
+});
