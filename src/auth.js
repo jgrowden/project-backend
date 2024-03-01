445ee3917732e9575ccfbc3472fb73e80850f6d1
Kaelan <z5478371@ad.unsw.edu.au>
2024-03-02 00:59:33 +1100

    Feat + Test: Implemented adminUserDetailsUpdate function and fixed tests

--- src/auth.js
+++ src/auth.js
@@ -174,9 +174,57 @@ export function adminUserDetails(authUserId) {
  * 
  * @return {} - an empty object
 */
-function adminUserDetailsUpdate(authUserId, email, nameFirst, nameLast) {
+export function adminUserDetailsUpdate(authUserId, email, nameFirst, nameLast) {
 
-    return {};
+    if (typeof authUserId !== "number") {
+        return { error: 'Invalid ID' };
+    }
+    if (validator.isEmail(email) === false) {
+        return { error: 'Invalid email' };
+    }
+
+    // name length check
+    if (nameFirst.length < 2) {
+        return { error: 'Names should be 2 or more characters' }
+    }
+    if (nameFirst.length > 20) {
+        return { error: 'Names should be 20 or less characters' }
+    }
+    if (nameLast.length < 2) {
+        return { error: 'Names should be 2 or more characters' }
+    }
+    if (nameLast.length > 20) {
+        return { error: 'Names should be 20 or less characters' }
+    }
+
+    const validChars = createValidCharsArray();
+
+    // Check name validity
+    if (validator.isWhitelisted(nameFirst, validChars) == false) {
+        return { error: 'Invalid first name' };
+    }
+    if (validator.isWhitelisted(nameLast, validChars) == false) {
+        return { error: 'Invalid last name' };
+    }
+
+    const data = getData();
+
+    for (let user of data.users) {
+        if (user.email === email) {
+            return { error: 'Email already registered'};
+        }
+    }
+
+    for (let user of data.users) {
+        if (user.authUserId === authUserId) {
+            user.email = email;
+            user.nameFirst = nameFirst;
+            user.nameLast = nameLast;
+            return {};
+        }
+    }
+
+    return { error: 'User ID not found'};
 }
 
 /**
--- src/tests/adminUserDetailsUpdate.test.js
+++ src/tests/adminUserDetailsUpdate.test.js
@@ -15,81 +15,97 @@ AuthUserId is not a valid user.
 function adminUserDetailsUpdate(authUserId, email, nameFirst, nameLast) {
 */
 
-import { clear } from "console";
+import { clear } from "../other.js";
 import { adminAuthRegister, adminUserDetails, adminUserDetailsUpdate } from "../auth";
 import { getData } from "../dataStore";
 
+describe('adminUserDetailsUpdate testing', () => {
+    
     let user1;
     let user2;
 
-beforeEach(() => {
-    clear();
-    user1 = adminAuthRegister('go.d.usopp@gmail.com', 'S0geking', 'God', 'Usopp');
-    user2 = adminAuthRegister('doffy@gmail.com', 'String-Str1ng', 'Donquixote', 'Doflamingo');
-});
+    beforeEach(() => {
+        clear();
+        user1 = adminAuthRegister('go.d.usopp@gmail.com', 'S0geking', 'God', 'Usopp');
+        user2 = adminAuthRegister('doffy@gmail.com', 'String-Str1ng', 'Donquixote', 'Doflamingo');
+    });
 
-describe('adminUserDetailsUpdate testing', () => {
     test('invalid ID', () => {
         expect(adminUserDetailsUpdate("invalid id", "test@email.com", "John", "Smith"))
         .toMatchObject({ 'error': 'Invalid ID' });
     });
+    
     test('ID does not exist', () => {
         expect(adminUserDetailsUpdate(5, "test@email.com", "Amog", "Us"))
-        .toMatchObject({ 'error': 'User with given ID not found' });
+        .toMatchObject({ 'error': 'User ID not found' });
     });
+
     test('email used by another user', () => {
         expect(adminUserDetailsUpdate(user2.authUserId, "go.d.usopp@gmail.com", "John", "Smith"))
-        .toMatchObject({ 'error': 'Email in use by another user' });
+        .toMatchObject({ 'error': 'Email already registered' });
     });
+
     test('email does not satisfy validator', () => {
         expect(adminUserDetailsUpdate(user2.authUserId, "not_an_email", "John", "Smith"))
-        .toMatchObject({ 'error': 'Invalid Email' });
+        .toMatchObject({ 'error': 'Invalid email' });
     });
+
     test('invalid characters in nameFirst 1', () => {
         expect(adminUserDetailsUpdate(user2.authUserId, "test@email.com", "Jo!hn", "Smith"))
         .toMatchObject({ 'error': 'Invalid first name' });
     });
+
     test('invalid characters in nameFirst 2', () => {
         expect(adminUserDetailsUpdate(user2.authUserId, "test@email.com", "J0hn", "Smith"))
         .toMatchObject({ 'error': 'Invalid first name' });
     });
+
     test('nameFirst less than 2 characters long', () => {
         expect(adminUserDetailsUpdate(user2.authUserId, "test@email.com", "J", "Smith"))
-        .toMatchObject({ 'error': 'First name too short' });
+        .toMatchObject({ 'error': 'Names should be 2 or more characters' });
     });
+
     test('nameFirst more than 20 characters long', () => {
         expect(adminUserDetailsUpdate(user2.authUserId, "test@email.com", "Abcdefghijklmnopqrstuvwxyz", "Smith"))
-        .toMatchObject({ 'error': 'First name too long' });
+        .toMatchObject({ 'error': 'Names should be 20 or less characters' });
     });
+
     test('invalid characters in nameLast 1', () => {
         expect(adminUserDetailsUpdate(user2.authUserId, "test@email.com", "John", "Sm1th"))
         .toMatchObject({ 'error': 'Invalid last name' });
 
     });
+
     test('invalid characters in nameLast 1', () => {
         expect(adminUserDetailsUpdate(user2.authUserId, "test@email.com", "John", "Sm@th"))
         .toMatchObject({ 'error': 'Invalid last name' });
     });
+
     test('nameLast less than 2 characters', () => {
         expect(adminUserDetailsUpdate(user2.authUserId, "test@email.com", "John", "S"))
-        .toMatchObject({ 'error': 'Last name too short' });
+        .toMatchObject({ 'error': 'Names should be 2 or more characters' });
     });
+
     test('nameLast more than 20 characters', () => {
         expect(adminUserDetailsUpdate(user2.authUserId, "test@email.com", "John", "Abcdefghijklmnopqrstuvwxyz"))
-        .toMatchObject({ 'error': 'Last name too long' });
+        .toMatchObject({ 'error': 'Names should be 20 or less characters' });
     });
+
     test('no errors 1', () => {
         expect(JSON.stringify(adminUserDetailsUpdate(user2.authUserId, "test@email.com", "John", "Smith"))).toBe('{}');
         console.log(getData);
     });
+
     test('no errors 2', () => {
         expect(JSON.stringify(adminUserDetailsUpdate(user1.authUserId, "test2@email.com", "Johnny", "Smitho"))).toBe('{}');
         console.log(getData());
     });
+
     test('no errors 3, 20 character names', () => {
         expect(JSON.stringify(adminUserDetailsUpdate(user1.authUserId, "test3@email.com", "abcdefghijklmnopqrst", "tsrqponmlkjihgfedcab"))).toBe('{}');
         console.log(getData());
     });
+
     test('no errors 4, 2 character names', () => {
         expect(JSON.stringify(adminUserDetailsUpdate(user1.authUserId, "test4@email.com", "ab", "ba"))).toBe('{}');
         console.log(getData());
