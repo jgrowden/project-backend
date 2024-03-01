bd07a898647cbc0838320560a2fca914a6f5dfbd
Kaelan <z5478371@ad.unsw.edu.au>
2024-03-01 23:09:15 +1100

    test: cleaned repetition in adminUserDetailsUpdate

--- src/tests/adminUserDetailsUpdate.test.js
+++ src/tests/adminUserDetailsUpdate.test.js
@@ -19,91 +19,55 @@ import { clear } from "console";
 import { adminAuthRegister, adminUserDetails, adminUserDetailsUpdate } from "../auth";
 import { getData } from "../dataStore";
 
+beforeEach(() => {
+    clear();
+    const user1 = adminAuthRegister('go.d.usopp@gmail.com', 'S0geking', 'God', 'Usopp');
+    const user2 = adminAuthRegister('doffy@gmail.com', 'String-Str1ng', 'Donquixote', 'Doflamingo');
+});
+
 describe('adminUserDetailsUpdate testing', () => {
     test('invalid ID', () => {
-        clear();
-        const user1 = adminAuthRegister('go.d.usopp@gmail.com', 'S0geking', 'God', 'Usopp');
-        const user2 = adminAuthRegister('doffy@gmail.com', 'String-Str1ng', 'Donquixote', 'Doflamingo');
         expect(adminUserDetailsUpdate("invalid id", "test@email.com", "John", "Smith")).toHaveProperty("error");
     });
     test('ID does not exist', () => {
-        clear();
-        const user1 = adminAuthRegister('go.d.usopp@gmail.com', 'S0geking', 'God', 'Usopp');
-        const user2 = adminAuthRegister('doffy@gmail.com', 'String-Str1ng', 'Donquixote', 'Doflamingo');
         expect(adminUserDetailsUpdate(5, "test@email.com", "Amog", "Us")).toHaveProperty("error");
     });
     test('email used by another user', () => {
-        clear();
-        const user1 = adminAuthRegister('go.d.usopp@gmail.com', 'S0geking', 'God', 'Usopp');
-        const user2 = adminAuthRegister('doffy@gmail.com', 'String-Str1ng', 'Donquixote', 'Doflamingo');
         expect(adminUserDetailsUpdate(user2.authUserId, "go.d.usopp@gmail.com", "John", "Smith")).toHaveProperty("error");
     });
     test('email does not satisfy validator', () => {
-        clear();
-        const user1 = adminAuthRegister('go.d.usopp@gmail.com', 'S0geking', 'God', 'Usopp');
-        const user2 = adminAuthRegister('doffy@gmail.com', 'String-Str1ng', 'Donquixote', 'Doflamingo');
         expect(adminUserDetailsUpdate(user2.authUserId, "not_an_email", "John", "Smith")).toHaveProperty("error");
     });
     test('invalid characters in nameFirst 1', () => {
-        clear();
-        const user1 = adminAuthRegister('go.d.usopp@gmail.com', 'S0geking', 'God', 'Usopp');
-        const user2 = adminAuthRegister('doffy@gmail.com', 'String-Str1ng', 'Donquixote', 'Doflamingo');
         expect(adminUserDetailsUpdate(user2.authUserId, "test@email.com", "Jo!hn", "Smith")).toHaveProperty("error");
     });
     test('invalid characters in nameFirst 2', () => {
-        clear();
-        const user1 = adminAuthRegister('go.d.usopp@gmail.com', 'S0geking', 'God', 'Usopp');
-        const user2 = adminAuthRegister('doffy@gmail.com', 'String-Str1ng', 'Donquixote', 'Doflamingo');
         expect(adminUserDetailsUpdate(user2.authUserId, "test@email.com", "J0hn", "Smith")).toHaveProperty("error");
     });
     test('nameFirst less than 2 characters long', () => {
-        clear();
-        const user1 = adminAuthRegister('go.d.usopp@gmail.com', 'S0geking', 'God', 'Usopp');
-        const user2 = adminAuthRegister('doffy@gmail.com', 'String-Str1ng', 'Donquixote', 'Doflamingo');
         expect(adminUserDetailsUpdate(user2.authUserId, "test@email.com", "J", "Smith")).toHaveProperty("error");
     });
     test('nameFirst more than 20 characters long', () => {
-        clear();
-        const user1 = adminAuthRegister('go.d.usopp@gmail.com', 'S0geking', 'God', 'Usopp');
-        const user2 = adminAuthRegister('doffy@gmail.com', 'String-Str1ng', 'Donquixote', 'Doflamingo');
         expect(adminUserDetailsUpdate(user2.authUserId, "test@email.com", "Abcdefghijklmnopqrstuvwxyz", "Smith")).toHaveProperty("error");
     });
     test('invalid characters in nameLast 1', () => {
-        clear();
-        const user1 = adminAuthRegister('go.d.usopp@gmail.com', 'S0geking', 'God', 'Usopp');
-        const user2 = adminAuthRegister('doffy@gmail.com', 'String-Str1ng', 'Donquixote', 'Doflamingo');
         expect(adminUserDetailsUpdate(user2.authUserId, "test@email.com", "John", "Sm1th")).toHaveProperty("error");
 
     });
     test('invalid characters in nameLast 1', () => {
-        clear();
-        const user1 = adminAuthRegister('go.d.usopp@gmail.com', 'S0geking', 'God', 'Usopp');
-        const user2 = adminAuthRegister('doffy@gmail.com', 'String-Str1ng', 'Donquixote', 'Doflamingo');
         expect(adminUserDetailsUpdate(user2.authUserId, "test@email.com", "John", "Sm@th")).toHaveProperty("error");
     });
     test('nameLast less than 2 characters', () => {
-        clear();
-        const user1 = adminAuthRegister('go.d.usopp@gmail.com', 'S0geking', 'God', 'Usopp');
-        const user2 = adminAuthRegister('doffy@gmail.com', 'String-Str1ng', 'Donquixote', 'Doflamingo');
         expect(adminUserDetailsUpdate(user2.authUserId, "test@email.com", "John", "S")).toHaveProperty("error");
     });
     test('nameLast more than 20 characters', () => {
-        clear();
-        const user1 = adminAuthRegister('go.d.usopp@gmail.com', 'S0geking', 'God', 'Usopp');
-        const user2 = adminAuthRegister('doffy@gmail.com', 'String-Str1ng', 'Donquixote', 'Doflamingo');
         expect(adminUserDetailsUpdate(user2.authUserId, "test@email.com", "John", "Abcdefghijklmnopqrstuvwxyz")).toHaveProperty("error");
     });
     test('no errors 1', () => {
-        clear();
-        const user1 = adminAuthRegister('go.d.usopp@gmail.com', 'S0geking', 'God', 'Usopp');
-        const user2 = adminAuthRegister('doffy@gmail.com', 'String-Str1ng', 'Donquixote', 'Doflamingo');
         expect(JSON.stringify(adminUserDetailsUpdate(user2.authUserId, "test@email.com", "John", "Smith"))).toBe('{}');
         console.log(getData);
     });
     test('no errors 2', () => {
-        clear();
-        const user1 = adminAuthRegister('go.d.usopp@gmail.com', 'S0geking', 'God', 'Usopp');
-        const user2 = adminAuthRegister('doffy@gmail.com', 'String-Str1ng', 'Donquixote', 'Doflamingo');
         expect(JSON.stringify(adminUserDetailsUpdate(user1.authUserId, "test2@email.com", "Johnny", "Smitho"))).toBe('{}');
         console.log(getData());
     });
