843faec634be8c872733398737dfa21414629f08
James Gair <z5360800@ad.unsw.edu.au>
2024-03-06 18:06:06 +1100

    All testing fixed and bugs squashed

--- src/tests/adminQuizNameUpdate.test.js
+++ src/tests/adminQuizNameUpdate.test.js
@@ -1,4 +1,5 @@
 import { adminQuizCreate, adminQuizNameUpdate } from '../quiz.js'
+import { getData } from '../dataStore.js'
 import { clear } from '../other.js'
 import { adminAuthRegister } from '../auth.js'
 
@@ -10,7 +11,7 @@ beforeEach(() => {
 describe('adminQuizNameUpdate', () => { 
 
     test('invalid user ID', () => {
-        expect(adminQuizNameUpdate(-1, 1, 'great quiz'))
+        expect(adminQuizNameUpdate('user ID', 1, 'great quiz'))
             .toStrictEqual({ 'error': 'Invalid user ID' });
     });
 
@@ -23,7 +24,7 @@ describe('adminQuizNameUpdate', () => {
     test('invalid quiz ID', () => { 
         let user1 = adminAuthRegister('go.d.usopp@gmail.com', 'S0geking', 'God', 'Usopp');
         let quiz1 = adminQuizCreate(user1.authUserId, 'good quiz', 'is good');   
-        expect(adminQuizNameUpdate(user1.authUserId, 'quizID', 'great quiz'))
+        expect(adminQuizNameUpdate(user1.authUserId, 'quizId', 'great quiz'))
             .toStrictEqual({ 'error': 'Invalid quiz ID' });
     });
 
@@ -36,30 +37,30 @@ describe('adminQuizNameUpdate', () => {
 
     test('quiz not owned by user', () => {  
         let user1 = adminAuthRegister('go.d.usopp@gmail.com', 'S0geking', 'God', 'Usopp');
-        let quiz1 = adminQuizCreate(user1.authUserId, 'good quiz', 'is good');  
+        let quiz1 = adminQuizCreate(user1.authUserId, 'good quiz', 'is good'); 
         let user2 = adminAuthRegister('fancypants@gmail.com', 'f4ncyP4nts', 'Fancy', 'Pants');
-        expect(adminQuizNameUpdate(user2.authUserId, quiz1.quizID, 'great quiz'))
+        expect(adminQuizNameUpdate(user2.authUserId, quiz1.quizId, 'great quiz'))
             .toStrictEqual({ 'error': 'Quiz not owned by user' });
     });
 
     test('invalid characters in quiz name', () => {  
         let user1 = adminAuthRegister('go.d.usopp@gmail.com', 'S0geking', 'God', 'Usopp');
         let quiz1 = adminQuizCreate(user1.authUserId, 'good quiz', 'is good');  
-        expect(adminQuizNameUpdate(user1.authUserId, quiz1.quizID, '/!greatQUIZ>>'))
+        expect(adminQuizNameUpdate(user1.authUserId, quiz1.quizId, '/!greatQUIZ>>'))
             .toStrictEqual({ 'error': 'Invalid characters found in quiz name' });
     });
 
     test('quiz name less than 3 characters long', () => {   
         let user1 = adminAuthRegister('go.d.usopp@gmail.com', 'S0geking', 'God', 'Usopp');
         let quiz1 = adminQuizCreate(user1.authUserId, 'good quiz', 'is good'); 
-        expect(adminQuizNameUpdate(user1.authUserId, quiz1.quizID, 'ab'))
+        expect(adminQuizNameUpdate(user1.authUserId, quiz1.quizId, 'ab'))
             .toStrictEqual({ 'error': 'Quiz name should be more than 3 characters' });
     });
 
     test('quiz name more than 30 characters long', () => {    
         let user1 = adminAuthRegister('go.d.usopp@gmail.com', 'S0geking', 'God', 'Usopp');
         let quiz1 = adminQuizCreate(user1.authUserId, 'good quiz', 'is good');
-        expect(adminQuizNameUpdate(user1.authUserId, quiz1.quizID, 'abcedfghijklmnopqrstuvwxyz123456789'))
+        expect(adminQuizNameUpdate(user1.authUserId, quiz1.quizId, 'abcedfghijklmnopqrstuvwxyz123456789'))
             .toStrictEqual({ 'error': 'Quiz name should be less than 30 characters' });
     });
 
@@ -67,28 +68,28 @@ describe('adminQuizNameUpdate', () => {
         let user1 = adminAuthRegister('go.d.usopp@gmail.com', 'S0geking', 'God', 'Usopp');
         let quiz1 = adminQuizCreate(user1.authUserId, 'good quiz', 'is good'); 
         let quiz2 = adminQuizCreate(user1.authUserId, 'great quiz', 'is good');
-        expect(adminQuizNameUpdate(user1.authUserId, quiz1.quizID, 'great quiz'))
+        expect(adminQuizNameUpdate(user1.authUserId, quiz1.quizId, 'great quiz'))
             .toStrictEqual({ 'error': 'Quiz name already taken' });
     });
 
     test('no errors 1', () => {
         let user1 = adminAuthRegister('go.d.usopp@gmail.com', 'S0geking', 'God', 'Usopp');
         let quiz1 = adminQuizCreate(user1.authUserId, 'good quiz', 'is good');
-        expect(JSON.stringify(adminQuizNameUpdate(user1.authUserId, quiz1.quizID, 'great quiz'))).toBe('{}');
+        expect(JSON.stringify(adminQuizNameUpdate(user1.authUserId, quiz1.quizId, 'great quiz'))).toBe('{}');
         console.log(getData);
     });
 
     test('no errors 2, 3 character names', () => {
         let user1 = adminAuthRegister('go.d.usopp@gmail.com', 'S0geking', 'God', 'Usopp');
         let quiz1 = adminQuizCreate(user1.authUserId, 'good quiz', 'is good');
-        expect(JSON.stringify(adminQuizNameUpdate(user1.authUserId, quiz1.quizID, 'Abc'))).toBe('{}');
+        expect(JSON.stringify(adminQuizNameUpdate(user1.authUserId, quiz1.quizId, 'Abc'))).toBe('{}');
         console.log(getData);
     });
 
     test('no errors 3, 30 character names', () => {
         let user1 = adminAuthRegister('go.d.usopp@gmail.com', 'S0geking', 'God', 'Usopp');
         let quiz1 = adminQuizCreate(user1.authUserId, 'good quiz', 'is good');
-        expect(JSON.stringify(adminQuizNameUpdate(user1.authUserId, quiz1.quizID, 'ABCdefghijklmnopqrstuvwxyz1234'))).toBe('{}');
+        expect(JSON.stringify(adminQuizNameUpdate(user1.authUserId, quiz1.quizId, 'ABCdefghijklmnopqrstuvwxyz1234'))).toBe('{}');
         console.log(getData);
     });
 
