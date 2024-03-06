25407ee3af75efa01a0f4723b3fb4108dd28fdf8
Andrew <z5487663@ad.unsw.edu.au>
2024-03-06 14:23:08 +1100

    impl: implemented adminQuizRemove function, note that not all the tests currently work because adminQuizCreate does not work in this branch (hasn't been implemented): merge before final testing.

---
+++ src/tests/adminQuizRemove.test.js
@@ -0,0 +1,36 @@
+import { adminQuizCreate, adminQuizRemove } from '../quiz.js'
+import { clear } from '../other.js'
+import { adminAuthRegister } from '../auth.js'
+
+clear();
+beforeEach(() => {
+    clear();
+});
+
+describe('adminQuizRemove test cases', () => {
+    describe('success cases', () => {
+        test('correct return type', () => {
+            let user = adminAuthRegister('go.d.usopp@gmail.com', 'S0geking', 'God', 'Usopp');
+            let quiz = adminQuizCreate(user.authUserId, 'quiz name', 'quiz description');
+            expect(adminQuizRemove(user.authUserId, quiz.quizId)).toMatchObject({ });
+        })
+    })
+    describe('failure cases', () => {
+        test('check for invalid user ID', () => {
+            expect(adminQuizRemove(-1, 1))
+                .toMatchObject({ 'error': 'invalid user ID' });
+        })
+        test('Check for invalid quiz ID', () => {
+            let user = adminAuthRegister('go.d.usopp@gmail.com', 'S0geking', 'God', 'Usopp');
+            expect(adminQuizRemove(user.authUserId, -1))
+                .toMatchObject({ 'error': 'invalid quiz ID' });
+        })
+        test('Check for valid quiz ownership', () => {
+            let user1 = adminAuthRegister('go.d.usopp@gmail.com', 'S0geking', 'God', 'Usopp');
+            let user2 = adminAuthRegister('doffy@gmail.com', 'String-Str1ng', 'Donquixote', 'Doflamingo');
+            let user2quiz = adminQuizCreate(user2.authUserId, 'quiz name', 'quiz description');
+            expect(adminQuizRemove(user1.authUserId, user2quiz.quizId))
+                .toMatchObject({ 'error': 'you do not own this quiz' });
+        })
+    })
+})
