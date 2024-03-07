5856477545cd4dfb35a0e9f3f91978f97b946384
James Gair <z5360800@ad.unsw.edu.au>
2024-03-07 20:19:57 +1100

    Updated error string logic to work for any implentation of error message

--- src/tests/adminQuizNameUpdate.test.js
+++ src/tests/adminQuizNameUpdate.test.js
@@ -12,27 +12,27 @@ describe('adminQuizNameUpdate', () => {
 
     test('invalid user ID', () => {
         expect(adminQuizNameUpdate('user ID', 1, 'great quiz'))
-            .toStrictEqual({ 'error': 'Invalid user ID' });
+            .toStrictEqual( { error: expect.any(String) } );
     });
 
     test('user ID not found', () => {    
         clear();
         expect(adminQuizNameUpdate(1, 1, 'great quiz'))
-            .toStrictEqual({ 'error': 'User ID not found' });
+            .toStrictEqual( { error: expect.any(String) } );
     });
 
     test('invalid quiz ID', () => { 
         let user1 = adminAuthRegister('go.d.usopp@gmail.com', 'S0geking', 'God', 'Usopp');
         let quiz1 = adminQuizCreate(user1.authUserId, 'good quiz', 'is good');   
         expect(adminQuizNameUpdate(user1.authUserId, 'quizId', 'great quiz'))
-            .toStrictEqual({ 'error': 'Invalid quiz ID' });
+            .toStrictEqual( { error: expect.any(String) } );
     });
 
     test('quiz ID not found', () => { 
         clear();
         let user1 = adminAuthRegister('go.d.usopp@gmail.com', 'S0geking', 'God', 'Usopp');
         expect(adminQuizNameUpdate(user1.authUserId, 1, 'great quiz'))
-            .toStrictEqual({ 'error': 'Quiz ID not found' });
+            .toStrictEqual( { error: expect.any(String) } );
     });
 
     test('quiz not owned by user', () => {  
@@ -40,28 +40,28 @@ describe('adminQuizNameUpdate', () => {
         let quiz1 = adminQuizCreate(user1.authUserId, 'good quiz', 'is good'); 
         let user2 = adminAuthRegister('fancypants@gmail.com', 'f4ncyP4nts', 'Fancy', 'Pants');
         expect(adminQuizNameUpdate(user2.authUserId, quiz1.quizId, 'great quiz'))
-            .toStrictEqual({ 'error': 'Quiz not owned by user' });
+            .toStrictEqual( { error: expect.any(String) } );
     });
 
     test('invalid characters in quiz name', () => {  
         let user1 = adminAuthRegister('go.d.usopp@gmail.com', 'S0geking', 'God', 'Usopp');
         let quiz1 = adminQuizCreate(user1.authUserId, 'good quiz', 'is good');  
         expect(adminQuizNameUpdate(user1.authUserId, quiz1.quizId, '/!greatQUIZ>>'))
-            .toStrictEqual({ 'error': 'Invalid characters found in quiz name' });
+            .toStrictEqual( { error: expect.any(String) } );
     });
 
     test('quiz name less than 3 characters long', () => {   
         let user1 = adminAuthRegister('go.d.usopp@gmail.com', 'S0geking', 'God', 'Usopp');
         let quiz1 = adminQuizCreate(user1.authUserId, 'good quiz', 'is good'); 
         expect(adminQuizNameUpdate(user1.authUserId, quiz1.quizId, 'ab'))
-            .toStrictEqual({ 'error': 'Quiz name should be more than 3 characters' });
+            .toStrictEqual( { error: expect.any(String) } );
     });
 
     test('quiz name more than 30 characters long', () => {    
         let user1 = adminAuthRegister('go.d.usopp@gmail.com', 'S0geking', 'God', 'Usopp');
         let quiz1 = adminQuizCreate(user1.authUserId, 'good quiz', 'is good');
         expect(adminQuizNameUpdate(user1.authUserId, quiz1.quizId, 'abcedfghijklmnopqrstuvwxyz123456789'))
-            .toStrictEqual({ 'error': 'Quiz name should be less than 30 characters' });
+            .toStrictEqual( { error: expect.any(String) } );
     });
 
     test('quiz name already used on another quiz by logged in user', () => {   
@@ -69,7 +69,7 @@ describe('adminQuizNameUpdate', () => {
         let quiz1 = adminQuizCreate(user1.authUserId, 'good quiz', 'is good'); 
         let quiz2 = adminQuizCreate(user1.authUserId, 'great quiz', 'is good');
         expect(adminQuizNameUpdate(user1.authUserId, quiz1.quizId, 'great quiz'))
-            .toStrictEqual({ 'error': 'Quiz name already taken' });
+            .toStrictEqual( { error: expect.any(String) } );
     });
 
     test('no errors 1', () => {
