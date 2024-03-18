862a386acef172066f930dc0c84741b5e7b9fbb7
Andrew <z5487663@ad.unsw.edu.au>
2024-03-18 14:48:01 +1100

    fix: updated authRegister and authLogin functions to use requestHelper

--- src/tests/adminAuthLogin.test.ts
+++ src/tests/adminAuthLogin.test.ts
@@ -1,59 +1,30 @@
-import request from 'sync-request-curl';
-import { port, url } from '../config.json';
+import { requestAuthRegister, requestAuthLogin, clear, ERROR } from './wrapper';
 import { UserType } from '../dataStore';
 
-const SERVER_URL = `${url}:${port}`;
-const requestAuthRegister = (email: string, password: string, nameFirst: string, nameLast: string) => {
-  const res = request(
-    'POST',
-    SERVER_URL + '/v1/admin/auth/register',
-    {
-      json: {
-        email,
-        password,
-        nameFirst,
-        nameLast
-      }
-    }
-  );
-  return JSON.parse(res.body.toString());
-};
-const requestAuthLogin = (email: string, password: string) => {
-  const res = request(
-    'POST',
-    SERVER_URL + '/v1/admin/auth/login',
-    {
-      json: {
-        email,
-        password,
-      }
-    }
-  );
-  return JSON.parse(res.body.toString());
-};
-
-const ERROR = { error: expect.any(String) };
 beforeEach(() => {
-  request('DELETE', SERVER_URL + '/v1/clear', { json: {} });
+  clear();
 });
 
 describe('Testing adminAuthLogin', () => {
   let user: UserType;
   beforeEach(() => {
-    user = requestAuthRegister('gon.freecs@gmail.com', 'GonF1shing', 'Gon', 'Freecs');
+    const { jsonBody } = requestAuthRegister('gon.freecs@gmail.com', 'GonF1shing', 'Gon', 'Freecs');
+    user = jsonBody as UserType;
   });
   test('Test successful login', () => {
-    expect(requestAuthLogin('gon.freecs@gmail.com', 'GonF1shing'))
-      .toStrictEqual({ authUserId: user.authUserId });
+    expect(requestAuthLogin('gon.freecs@gmail.com', 'GonF1shing')).toStrictEqual({
+      statusCode: 200,
+      jsonBody: { authUserId: user.authUserId }
+    });
   });
 
   test('Test unsuccessful login with non-existent email', () => {
     expect(requestAuthLogin('killua@gmail.com', 'k1loWatt'))
-      .toStrictEqual(ERROR);
+      .toStrictEqual({ statusCode: 400, jsonBody: ERROR });
   });
 
   test('Test unsuccessful login with valid email, incorrect password', () => {
     expect(requestAuthLogin('gon.freecs@gmail.com', 'F1rstComesR0ck!'))
-      .toStrictEqual(ERROR);
+      .toStrictEqual({ statusCode: 400, jsonBody: ERROR });
   });
 });
---
+++ src/tests/wrapper.ts
@@ -0,0 +1,13 @@
+import { requestHelper } from './requestHelper';
+
+const requestAuthRegister = (email: string, password: string, nameFirst: string, nameLast: string) =>
+  requestHelper('POST', '/v1/admin/auth/register', { email, password, nameFirst, nameLast });
+
+const requestAuthLogin = (email: string, password: string) =>
+  requestHelper('POST', '/v1/admin/auth/login', { email, password });
+
+const clear = () => requestHelper('DELETE', '/v1/clear');
+
+const ERROR = { error: expect.any(String) };
+
+export { requestAuthRegister, requestAuthLogin, clear, ERROR };
