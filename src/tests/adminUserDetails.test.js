eed5d627330ec3a222a29d87dd8b80a2634ec5fc
Kaelan <z5478371@ad.unsw.edu.au>
2024-03-22 19:39:29 +1100

    tests: update tests for http testing

--- src/tests/adminUserDetails.test.js
+++ src/tests/adminUserDetails.test.js
@@ -1,5 +1,4 @@
-import { clear } from '../other';
-import { adminAuthRegister, adminUserDetails } from '../auth';
+import { clear, requestUserDetails, requestAuthRegister, ERROR } from './wrapper';
 
 beforeEach(() => {
   clear();
@@ -7,42 +6,61 @@ beforeEach(() => {
 
 describe('adminUserDetails testing', () => {
   test('empty database', () => {
-    expect(adminUserDetails(123456789)).toStrictEqual({ error: expect.any(String) });
+    expect(requestUserDetails(123456789)).toStrictEqual(
+      {
+        statusCode: 400,
+        jsonBody: ERROR
+      });
   });
   test('no token provided', () => {
-    expect(adminUserDetails()).toStrictEqual({ error: expect.any(String) });
-  });
-  test('invalid user id provided', () => {
-    expect(adminUserDetails('Nan')).toStrictEqual({ error: expect.any(String) });
+    expect(requestUserDetails(123456789)).toStrictEqual(
+      {
+        statusCode: 400,
+        jsonBody: ERROR
+      });
   });
   test('token does not exist', () => {
-    const user1 = adminAuthRegister('go.d.usopp@gmail.com', 'S0geking', 'God', 'Usopp');
-    expect(adminUserDetails(user1.token + '1')).toStrictEqual({ error: expect.any(String) });
+    const user1Token = requestAuthRegister('go.d.usopp@gmail.com', 'S0geking', 'God', 'Usopp').jsonBody.token;
+    expect(requestUserDetails(user1Token + '1')).toStrictEqual(
+      {
+        statusCode: 400,
+        jsonBody: ERROR
+      });
   });
   test('valid token 1', () => {
-    const user1 = adminAuthRegister('go.d.usopp@gmail.com', 'S0geking', 'God', 'Usopp');
-    adminAuthRegister('doffy@gmail.com', 'String-Str1ng', 'Donquixote', 'Doflamingo');
-    expect(adminUserDetails(user1.token)).toMatchObject({
-      user: {
-        userId: expect.any(Number),
-        name: 'God Usopp',
-        email: 'go.d.usopp@gmail.com',
-        numSuccessfulLogins: 1,
-        numFailedPasswordsSinceLastLogin: 0
+    const user1Token = requestAuthRegister('go.d.usopp@gmail.com', 'S0geking', 'God', 'Usopp').jsonBody.token;
+    requestAuthRegister('doffy@gmail.com', 'String-Str1ng', 'Donquixote', 'Doflamingo');
+    expect(requestUserDetails(user1Token)).toMatchObject(
+      {
+        statusCode: 200,
+        jsonBody: {
+          user: {
+            userId: expect.any(Number),
+            name: 'God Usopp',
+            email: 'go.d.usopp@gmail.com',
+            numSuccessfulLogins: 1,
+            numFailedPasswordsSinceLastLogin: 0
+          }
+        }
       }
-    });
+    );
   });
   test('valid token 2', () => {
-    adminAuthRegister('go.d.usopp@gmail.com', 'S0geking', 'God', 'Usopp');
-    const user2 = adminAuthRegister('doffy@gmail.com', 'String-Str1ng', 'Donquixote', 'Doflamingo');
-    expect(adminUserDetails(user2.token)).toMatchObject({
-      user: {
-        userId: expect.any(Number),
-        name: 'Donquixote Doflamingo',
-        email: 'doffy@gmail.com',
-        numSuccessfulLogins: 1,
-        numFailedPasswordsSinceLastLogin: 0
+    requestAuthRegister('go.d.usopp@gmail.com', 'S0geking', 'God', 'Usopp');
+    const user2Token = requestAuthRegister('doffy@gmail.com', 'String-Str1ng', 'Donquixote', 'Doflamingo').jsonBody.token;
+    expect(requestUserDetails(user2Token)).toMatchObject(
+      {
+        statusCode: 200,
+        jsonBody: {
+          user: {
+            userId: expect.any(Number),
+            name: 'Donquixote Doflamingo',
+            email: 'doffy@gmail.com',
+            numSuccessfulLogins: 1,
+            numFailedPasswordsSinceLastLogin: 0
+          }
+        }
       }
-    });
+    );
   });
 });
--- src/tests/wrapper.ts
+++ src/tests/wrapper.ts
@@ -6,8 +6,12 @@ const requestAuthRegister = (email: string, password: string, nameFirst: string,
 const requestAuthLogin = (email: string, password: string) =>
   requestHelper('POST', '/v1/admin/auth/login', { email, password });
 
+const requestUserDetails = (token: string) => {
+  
+};
+
 const clear = () => requestHelper('DELETE', '/v1/clear');
 
 const ERROR = { error: expect.any(String) };
 
-export { requestAuthRegister, requestAuthLogin, clear, ERROR };
+export { requestAuthRegister, requestAuthLogin, requestUserDetails, clear, ERROR };
