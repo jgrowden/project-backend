1bcb4d65b47c08c25bafd4cbe539948091f1ff05
James Growden <z5362762@ad.unsw.edu.au>
2024-03-23 00:52:03 +1100

    fix: removed merge conflict markers

--- src/tests/wrapper.ts
+++ src/tests/wrapper.ts
@@ -11,17 +11,15 @@ const requestAuthLogin = (email: string, password: string) =>
 const requestUserDetails = (token: string) =>
   requestHelper('GET', '/v1/admin/user/details', { token });
 
-<<<<<<< b13ab7135949f82e375bdb01883c2b6be9ecdc49
 const requestQuestionUpdate = (sessionId: string, quizId: number, questionId: number, questionBody: QuestionType) =>
   requestHelper('PUT', `/v1/admin/quiz/${quizId}/question/${questionId}`, { sessionId, questionBody });
 
 const clear = () => requestHelper('DELETE', '/v1/clear');
 
 export { requestAuthRegister, requestAuthLogin, requestUserDetails, requestQuestionUpdate, clear, ERROR };
-=======
+
 const clear = () => requestHelper('DELETE', '/v1/clear');
 
 const ERROR = { error: expect.any(String) };
 
 export { requestAuthRegister, requestAuthLogin, requestUserDetails, clear, ERROR };
->>>>>>> ac1af1299e80c30c20ce78f5f4ccf08da173d35d
