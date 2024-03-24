9589dce368dd76fd812ee273df83ac826df183b3
Kaelan <z5478371@ad.unsw.edu.au>
2024-03-24 14:57:31 +1100

    test: extra tests for authLogout

--- src/tests/iter2_fns/adminAuthLogout.test.ts
+++ src/tests/iter2_fns/adminAuthLogout.test.ts
@@ -1,6 +1,7 @@
-import { clear, errorCode, requestAuthLogout, requestAuthRegister } from '../wrapper';
+import { clear, errorCode, requestAuthLogin, requestAuthLogout, requestAuthRegister } from '../wrapper';
 
 let token1: string;
+let token2: string;
 
 beforeEach(() => {
   clear();
@@ -16,4 +17,13 @@ describe('adminAuthLogout tests for POST /v1/admin/auth/logout', () => {
     expect(requestAuthLogout(token1)).toStrictEqual({ statusCode: 200, jsonBody: {} });
     expect(requestAuthLogout(token1)).toStrictEqual(errorCode(401));
   });
+
+  test('logout, followed by another login and logout', () => {
+    expect(requestAuthLogout(token1)).toStrictEqual({ statusCode: 200, jsonBody: {} });
+    expect(requestAuthLogout(token1)).toStrictEqual(errorCode(401));
+    token2 = requestAuthLogin('hayden.smith@unsw.edu.au', 'haydensmith123').jsonBody.token;
+    expect(requestAuthLogout(token2)).toStrictEqual({ statusCode: 200, jsonBody: {} });
+    expect(requestAuthLogout(token2)).toStrictEqual(errorCode(401));
+    
+  })
 });
