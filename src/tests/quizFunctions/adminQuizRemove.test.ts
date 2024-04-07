e856e2dbb3c6d0120c46cf5cc98fbccc768261fa
Andrew <z5487663@ad.unsw.edu.au>
2024-04-07 23:09:28 +1000

    test: tests for quizRemoveV2 written.

--- src/tests/quizFunctions/adminQuizRemove.test.ts
+++ src/tests/quizFunctions/adminQuizRemove.test.ts
@@ -1,4 +1,5 @@
-import { requestAuthRegister, requestQuizCreate, requestQuizDelete, errorCode, clear } from '../wrapper';
+import { requestAuthRegister, requestQuizCreateV2, requestQuizDelete, requestQuizDeleteV2, errorCode, clear } from '../wrapper';
+import HTTPError from 'http-errors';
 
 let token: string;
 let quizId: number;
@@ -7,7 +8,7 @@ beforeEach(() => {
   clear();
   const user = requestAuthRegister('go.d.usopp@gmail.com', 'S0geking', 'God', 'Usopp');
   token = user.jsonBody.token as string;
-  const quiz = requestQuizCreate(token, 'Quiz Name', 'Quiz Description');
+  const quiz = requestQuizCreateV2(token, 'Quiz Name', 'Quiz Description');
   quizId = quiz.jsonBody.quizId as number;
 });
 
@@ -28,5 +29,25 @@ describe('Testing /v1/admin/quiz/{quizid}:', () => {
     const { jsonBody } = requestAuthRegister('doffy@gmail.com', 'String-Str1ng', 'Donquixote', 'Doflamingo');
     const otherToken = jsonBody.token as string;
     expect(requestQuizDelete(otherToken, quizId)).toStrictEqual(errorCode(403));
+  });
+});
+
+describe('Testing /v1/admin/quiz/{quizid}:', () => {
+  test('Successful test.', () => {
+    expect(requestQuizDeleteV2(token, quizId)).toStrictEqual({
+      statusCode: 200,
+      jsonBody: {}
+    });
+  });
+  test('Failed test: user does not exist.', () => {
+    expect(requestQuizDeleteV2(token + 'a', quizId)).toThrow(HTTPError[401]);
+  });
+  test('Failed test: quiz does not exist.', () => {
+    expect(requestQuizDeleteV2(token, quizId + 1)).toThrow(HTTPError[403]);
+  });
+  test('Failed test: user provided does not own quiz.', () => {
+    const { jsonBody } = requestAuthRegister('doffy@gmail.com', 'String-Str1ng', 'Donquixote', 'Doflamingo');
+    const otherToken = jsonBody.token as string;
+    expect(requestQuizDeleteV2(otherToken, quizId)).toThrow(HTTPError[403]);
   });
 });
