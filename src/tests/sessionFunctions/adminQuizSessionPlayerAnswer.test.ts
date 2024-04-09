b6b3464c8d116d59c2f2cb7fb77025314eb837de
Andrew <z5487663@ad.unsw.edu.au>
2024-04-10 01:19:57 +1000

    test: tests for playerAnswer implemented.

---
+++ src/tests/sessionFunctions/adminQuizSessionPlayerAnswer.test.ts
@@ -0,0 +1,66 @@
+import HTTPError from 'http-errors';
+import {
+    requestAuthRegister,
+    requestQuizCreateV2,
+    requestQuizQuestionCreateV2,
+    requestQuizSessionStart,
+    requestQuizSessionPlayerJoin,
+    requestQuizSessionPlayerAnswer,
+    clear
+} from '../wrapper';
+
+let token: string;
+let quizId: number;
+let sessionId: number;
+const AUTOSTARTNUM = 10;
+let playerId: number;
+let answerId: number;
+
+beforeEach(() => {
+    clear();
+    token = requestAuthRegister('go.d.usopp@gmail.com', 'S0geking', 'God', 'Usopp').jsonBody.token as string;
+    quizId = requestQuizCreateV2(token, 'Quiz Name', 'Quiz Description').jsonBody.quizId as number;
+    requestQuizQuestionCreateV2(token, quizId, {
+        question: 'How tall am I?',
+        duration: 5,
+        points: 4,
+        answers: [{ answer: 'Answer!', correct: true }, { answer: 'Another Answer!', correct: true }],
+        thumbnailUrl: 'http://example.com/birb.jpg'
+      });
+    sessionId = requestQuizSessionStart(token, quizId, AUTOSTARTNUM).jsonBody.sessionId as number;
+    playerId = requestQuizSessionPlayerJoin(sessionId, 'John Smith').jsonBody.playerId as number;
+    let data = requestQuizInfoV2(token, quizId).jsonBody;
+    answerId = data.questions[0].answers[0].answerId;
+    // update quiz session state
+});
+
+describe('Tests for PUT /v1/player/{playerid}/question/{questionposition}/answer', () => {
+    test('Success', () => {
+        expect(requestQuizSessionPlayerAnswer(playerId, 1, [answerId])).toStrictEqual({
+            statusCode: 200,
+            jsonBody: {}
+        });
+    });
+    test('Fail: player id does not exist', () => {
+        expect(() => requestQuizSessionPlayerAnswer(playerId + 1, 1, [answerId])).toThrow(HTTPError[400]);
+    });
+    test('Fail: invalid question position', () => {
+        expect(() => requestQuizSessionPlayerAnswer(playerId, -1, [answerId])).toThrow(HTTPError[400]);
+    });
+    test('Fail: session is not in QUESTION_OPEN state', () => {
+        // update state
+        expect(() => requestQuizSessionPlayerAnswer(playerId, 1, [answerId])).toThrow(HTTPError[400]);
+    });
+    test('Fail: session is not up to this question yet', () => {
+        expect(() => requestQuizSessionPlayerAnswer(playerId, 2, [answerId])).toThrow(HTTPError[400]);
+    });
+    test('Fail: invalid answerId for this question', () => {
+        expect(() => requestQuizSessionPlayerAnswer(playerId, 1, [answerId + 1])).toThrow(HTTPError[400]);
+    });
+    test('Fail: duplicate answer ids provided', () => {
+        expect(() => requestQuizSessionPlayerAnswer(playerId, 1, [answerId, answerId])).toThrow(HTTPError[400]);
+    });
+    test('Fail: Not enough answer ids provided (1)', () => {
+        expect(() => requestQuizSessionPlayerAnswer(playerId, 1, [])).toThrow(HTTPError[400]);
+    });
+});
