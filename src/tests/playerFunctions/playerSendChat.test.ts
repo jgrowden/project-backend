83e87e56590f40ae2cc116de33175d646305251b
James Gair <z5360800@ad.unsw.edu.au>
2024-04-18 13:15:55 +1000

    refactoring

--- src/tests/playerFunctions/playerSendChat.test.ts
+++ src/tests/playerFunctions/playerSendChat.test.ts
@@ -15,9 +15,31 @@ let sessionId1: number;
 let player1: number;
 let player2: number;
 let player3: number;
-let questionBody1: QuestionType;
-let questionBody2: QuestionType;
-let questionBody3: QuestionType;
+
+const questionBody1: QuestionType = {
+  question: 'Who is the imposter?',
+  duration: 1,
+  points: 10,
+  answers: [{ answer: 'Red', correct: false }, { answer: 'Blue', correct: false }, { answer: 'Green', correct: false }, { answer: 'Orange', correct: true }],
+  thumbnailUrl: 'http://sus.com/sus.jpg'
+};
+
+const questionBody2: QuestionType = {
+  question: 'Why last vented in electrical?',
+  duration: 2,
+  points: 6,
+  answers: [{ answer: 'Red', correct: true }, { answer: 'Blue', correct: false }, { answer: 'Green', correct: false }, { answer: 'Orange', correct: false }],
+  thumbnailUrl: 'http://alsosus.com/sus.jpg'
+};
+
+const questionBody3: QuestionType = {
+  question: 'How many imposters are left?',
+  duration: 10,
+  points: 8,
+  answers: [{ answer: '1', correct: true }, { answer: '2', correct: false }, { answer: '3', correct: false }, { answer: '4', correct: false }],
+  thumbnailUrl: 'http://alsosus.com/sus.jpg'
+};
+
 const AUTOSTARTNUM = 10;
 
 describe('playerSendChat testing', () => {
@@ -27,81 +49,6 @@ describe('playerSendChat testing', () => {
 
     const quiz1 = requestQuizCreateV2(token1, 'Quiz Name', 'Quiz Description');
     quizId1 = quiz1.jsonBody.quizId as number;
-
-    questionBody1 = {
-      question: 'Who is the imposter?',
-      duration: 1,
-      points: 10,
-      answers: [
-        {
-          answer: 'Red',
-          correct: false,
-        },
-        {
-          answer: 'Blue',
-          correct: false,
-        },
-        {
-          answer: 'Green',
-          correct: false,
-        },
-        {
-          answer: 'Orange',
-          correct: true,
-        },
-      ],
-      thumbnailUrl: 'http://sus.com/sus.jpg'
-    };
-
-    questionBody2 = {
-      question: 'Why last vented in electrical?',
-      duration: 2,
-      points: 6,
-      answers: [
-        {
-          answer: 'Red',
-          correct: true,
-        },
-        {
-          answer: 'Blue',
-          correct: false,
-        },
-        {
-          answer: 'Green',
-          correct: false,
-        },
-        {
-          answer: 'Orange',
-          correct: false,
-        },
-      ],
-      thumbnailUrl: 'http://alsosus.com/sus.jpg'
-    };
-
-    questionBody3 = {
-      question: 'How many imposters are left?',
-      duration: 10,
-      points: 8,
-      answers: [
-        {
-          answer: '1',
-          correct: true,
-        },
-        {
-          answer: '2',
-          correct: false,
-        },
-        {
-          answer: '3',
-          correct: false,
-        },
-        {
-          answer: '4',
-          correct: false,
-        },
-      ],
-      thumbnailUrl: 'http://alsosus.com/sus.jpg'
-    };
 
     requestQuizQuestionCreateV2(token1, quizId1, questionBody1).jsonBody.questionId as number;
     requestQuizQuestionCreateV2(token1, quizId1, questionBody2).jsonBody.questionId as number;
@@ -120,81 +67,6 @@ describe('playerSendChat testing', () => {
 
     const quiz1 = requestQuizCreateV2(token1, 'Quiz Name', 'Quiz Description');
     quizId1 = quiz1.jsonBody.quizId as number;
-
-    questionBody1 = {
-      question: 'Who is the imposter?',
-      duration: 1,
-      points: 10,
-      answers: [
-        {
-          answer: 'Red',
-          correct: false,
-        },
-        {
-          answer: 'Blue',
-          correct: false,
-        },
-        {
-          answer: 'Green',
-          correct: false,
-        },
-        {
-          answer: 'Orange',
-          correct: true,
-        },
-      ],
-      thumbnailUrl: 'http://sus.com/sus.jpg'
-    };
-
-    questionBody2 = {
-      question: 'Why last vented in electrical?',
-      duration: 2,
-      points: 6,
-      answers: [
-        {
-          answer: 'Red',
-          correct: true,
-        },
-        {
-          answer: 'Blue',
-          correct: false,
-        },
-        {
-          answer: 'Green',
-          correct: false,
-        },
-        {
-          answer: 'Orange',
-          correct: false,
-        },
-      ],
-      thumbnailUrl: 'http://alsosus.com/sus.jpg'
-    };
-
-    questionBody3 = {
-      question: 'How many imposters are left?',
-      duration: 10,
-      points: 8,
-      answers: [
-        {
-          answer: '1',
-          correct: true,
-        },
-        {
-          answer: '2',
-          correct: false,
-        },
-        {
-          answer: '3',
-          correct: false,
-        },
-        {
-          answer: '4',
-          correct: false,
-        },
-      ],
-      thumbnailUrl: 'http://alsosus.com/sus.jpg'
-    };
 
     requestQuizQuestionCreateV2(token1, quizId1, questionBody1).jsonBody.questionId as number;
     requestQuizQuestionCreateV2(token1, quizId1, questionBody2).jsonBody.questionId as number;
